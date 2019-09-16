354c6b613f704a377acd11258426df26962c00f3
Reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2019-09-16 17:21:41 +0100

    Make it work for node

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-import {config as DotEnvConfig} from "dotenv";
+const DiscordBot = require("./framework/DiscordBot");
+// if using glitch comment out the line below
+require('dotenv').config();
 
-import DiscordBot from "./framework/DiscordBot";
-
-DotEnvConfig();
-const bot = new DiscordBot(process.env.DISCORD_TOKEN).run();
+const bot = new DiscordBot(process.env.DISCORD_PREFIX, process.env.DISCORD_TOKEN).run();
--- framework/DiscordBot.js
+++ framework/DiscordBot.js
@@ -1,20 +1,22 @@
-import discord from "discord.js";
-import glob from "glob";
-import path from "path";
-import shlex from 'shlex';
-import Command from './core/Command';
+const discord = require("discord.js");
+const glob = require("glob");
+const path = require("path");
+const shlex = require('shlex');
+const Command = require('./core/Command');
 
 class DiscordBot {
-    #client;
-    #debug;
+    #client=null;
+    #debug=null;
     #commands = {};
     #listeners = {};
-    #voice;
-    #webserver;
+    #voice=null;
+    #webserver=null;
     #cache = {};
-    #token;
-    constructor(token) {
+    #token=null;
+    #prefix=null;
+    constructor(prefix, token) {
         this.#token = token;
+        this.#prefix = prefix;
     }
 
     run(startup_settings = {commands: true, listeners: true, voice: true}) {
@@ -23,29 +25,35 @@ class DiscordBot {
         this.#webserver = process.env.WEBSERVER;
         this._generate_commands();
         this._generate_listeners();
-        this.#client = discord.client(this.#token)
+        this.#client = new discord.Client();
         if (this.#debug) {
             this._on_connected();
         }
         this._on_message();
+        try{
+            this.#client.login(this.#token);
+        } catch (e) {
+            console.error(e.message);
+        }
+
     }
 
     _generate_commands() {
-        const commands = glob.sync('../commands/*.js').map(file => require( path.resolve( file ) ));
+        const commands = glob.sync('./commands/*.js').map(file => require( path.resolve( file )));
         let all_commands = {all: commands.map(c => {
-            try {
-                const t =  new c();
-                if (!(t instanceof Command)) {
+                try {
+                    const t =  new c();
+                    if (!(t instanceof Command)) {
+                        return false;
+                    }
+                    return t;
+                } catch (e) {
                     return false;
                 }
-                return t;
-            } catch (e) {
-                return false;
-            }
-        })};
+            })};
 
-        for (let command of commands){
-            all_commands[command.name] = {execute: command.execute, allowed_channels: command.allowed_channels};
+        for (let command of all_commands.all){
+            all_commands[command.name] = command;
         }
         this.#commands = all_commands;
     }
@@ -55,26 +63,27 @@ class DiscordBot {
     }
 
     _on_connected() {
-        this.#client.on('connected', () => {
+        this.#client.on('ready', () => {
             console.log(`Succesfully logged in as ${this.#client.user.tag}`)
         });
     }
 
     _on_message() {
-        this.#client.on('message', async message => {
-            if (message.author.bot || !message.content.startsWith('~')) return;
-            const args = shlex.split(message.content.slice(1).trim());
+        this.#client.on('message', async (message) => {
+            if (message.author.bot || !message.content.startsWith(this.#prefix)) return;
+            const args = shlex.split(message.content.slice(this.#prefix.length).trim());
             const command = args.shift().toLowerCase();
             let found_command = null;
-            for (let c of this.#commands) {
-                if (c.name === command || c.aliases.includes(command)) {
+            for (let [name, c] of Object.entries(this.#commands)) {
+                if (name === 'all') continue;
+                if (c.name === command || (c.aliases !== undefined && c.aliases.includes(command))) {
                     found_command = c;
-                    return;
+                    break;
                 }
             }
             if (found_command === null) return false;
             console.log(`Received command ${command} with params: ${JSON.stringify(args)}`);
-            if (this.#commands.allowed_channels.length !== 0 && !this.#commands[command].allowed_channels.includes(message.channel.id)) {
+            if (found_command.allowed_channels.length !== 0 && !found_command.allowed_channels.includes(message.channel.id)) {
                 console.log(`Command ${command} not allowed in channel ${message.channel.name}`);
                 return;
             }
@@ -83,4 +92,4 @@ class DiscordBot {
     }
 }
 
-export default DiscordBot;
+module.exports = DiscordBot;
