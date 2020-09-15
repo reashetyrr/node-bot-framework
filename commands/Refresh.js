8ac2a8082e509f093bd868669055f8204f7be254
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2020-09-15 12:20:32 +0200

    include refresh command

---
+++ commands/Refresh.js
@@ -0,0 +1,17 @@
+const Command = require("../framework/core/Command.js");
+
+class Refresh extends Command {
+    constructor() {
+        super();
+        this.name = 'refresh';
+        this.aliases = ['reload', 'rl'];
+        this.allowed_permissions = 'ADMINISTRATOR';
+    }
+
+    async execute(message, author, ...params) {
+        this.client.emit('custom.reload');
+        return message.reply('Succesfully reloaded commands and listeners');
+    }
+}
+
+module.exports = Refresh;
