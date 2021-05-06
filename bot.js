c51489503aa6e7b2c5ffcb5467fd3b828f136df4
reashetyrr <g.le.grand@hotmail.com>
2021-05-06 13:23:23 +0200

    check for discord token & profix, return with an error if no token is set default prefix to /

--- bot.js
+++ bot.js
@@ -2,4 +2,12 @@ const DiscordBot = require("./framework/DiscordBot");
 // if using glitch comment out the line below
 require('dotenv').config();
 
+if (!process.env.DISCORD_TOKEN)
+    return console.error('Missing bot token in .env');
+
+if (!process.env.DISCORD_PREFIX) {
+    console.error('Missing bot prefix, using / as default')
+    process.env.DISCORD_PREFIX = '/';
+}
+
 const bot = new DiscordBot(process.env.DISCORD_PREFIX, process.env.DISCORD_TOKEN).run();
