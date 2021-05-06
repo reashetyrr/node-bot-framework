a016f3940a127a6ec531eb12df848c7af0828337
reashetyrr <g.le.grand@hotmail.com>
2021-05-06 13:23:52 +0200

    alsoc heck for token in the start shardedmanager file

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 const { ShardingManager } = require('discord.js');
 require('dotenv').config();
 
+if (!process.env.DISCORD_TOKEN)
+    return console.error('Missing bot token in .env');
+
 const manager = new ShardingManager('./bot.js', {totalShards: 'auto', token: process.env.DISCORD_TOKEN, respawn: false});
 
 manager.spawn();
