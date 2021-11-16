eed73fdec62f01695d0c5f4bf6a022f0d28cbb48
reashetyrr <g.le.grand@hotmail.com>
2021-11-16 12:00:27 +0100

    derp

--- start.js
+++ start.js
@@ -1,10 +1,16 @@
 const { ShardingManager } = require('discord.js');
 require('dotenv').config();
 
-if (!process.env.DISCORD_TOKEN)
-    return console.error('Missing bot token in .env');
+const is_dev = process.env.hasOwnProperty('mode') && process.env.mode === 'dev'
+const token = is_dev? process.env.DISCORD_TOKEN_DEV : process.env.DISCORD_TOKEN_PROD;
 
-const manager = new ShardingManager('./bot.js', {totalShards: 'auto', token: process.env.DISCORD_TOKEN, respawn: false});
+if (!token && is_dev)
+    return console.error('Missing bot dev token in .env');
+
+if (!token && !is_dev)
+    return console.error('Missing bot prod token in .env');
+
+const manager = new ShardingManager('./bot.js', {totalShards: 'auto', token: token, respawn: false});
 
 manager.spawn();
 
